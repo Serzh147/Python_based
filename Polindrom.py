d01095468a13fd4ad07bf3dc4f1bcfbec344f663
Serzh147 <79854433620@yandex.ru>
2022-02-21 23:34:02 +0300

    new_task

---
+++ Polindrom.py
@@ -0,0 +1,8 @@
+number = 22022022
+
+
+# converting the number to string and then reversing it
+if str(number)[::-1] == str(number):
+    print("Number:", number, " is a Palindrome")
+else:
+    print("Number:", number, " is not a Palindrome")
