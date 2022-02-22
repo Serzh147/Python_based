c1eb122999e061c0528f0241ee67a27b3f004883
Serzh147 <79854433620@yandex.ru>
2022-02-22 08:11:19 +0300

    new_task

--- Anagramma.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-str1 = "кот"
-str2 = "ток"
-def anagrams(str1, str2):
-    return Counter(str1) == Counter(str2)
-anagrams("abc1", "1bac") # True
--- Polindrom.py
+++ Polindrom.py
@@ -6,3 +6,6 @@ if str(number)[::-1] == str(number):
     print("Number:", number, " is a Palindrome")
 else:
     print("Number:", number, " is not a Palindrome")
+
+
+
