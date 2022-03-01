33445b4f155f8a4b2cbff2f5ab472a5262d7105b
Serzh147 <79854433620@yandex.ru>
2022-03-02 00:04:46 +0300

    0302

---
+++ Practiks.py
@@ -0,0 +1,10 @@
+guests = ['ms.Tikhanovskaya','msr.Chaliy','msr.Bykov','msr.Armen','msr.Georgiy']
+#guest_person = guests[3]
+invite_text_intro = 'Dear '
+invite_text_body = '! I invite you to lounch!'
+#print(f'{invite_text_intro}{guest_person}{invite_tetxt_body}')
+print(f'{invite_text_intro}{guests[0]}{invite_text_body}')
+print(f'{invite_text_intro}{guests[1]}{invite_text_body}')
+print(f'{invite_text_intro}{guests[2]}{invite_text_body}')
+print(f'{invite_text_intro}{guests[3]}{invite_text_body}')
+print(f'{invite_text_intro}{guests[4]}{invite_text_body}')
