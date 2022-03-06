e5c8cb468ce1b7185843cc680f02aff41cf411e2
Serzh147 <79854433620@yandex.ru>
2022-03-06 08:28:30 +0300

    0306

--- Practiks.py
+++ Practiks.py
@@ -1,10 +1,15 @@
-guests = ['ms.Tikhanovskaya','msr.Chaliy','msr.Bykov','msr.Armen','msr.Georgiy']
-#guest_person = guests[3]
-invite_text_intro = 'Dear '
-invite_text_body = '! I invite you to lounch!'
-#print(f'{invite_text_intro}{guest_person}{invite_tetxt_body}')
-print(f'{invite_text_intro}{guests[0]}{invite_text_body}')
-print(f'{invite_text_intro}{guests[1]}{invite_text_body}')
-print(f'{invite_text_intro}{guests[2]}{invite_text_body}')
-print(f'{invite_text_intro}{guests[3]}{invite_text_body}')
-print(f'{invite_text_intro}{guests[4]}{invite_text_body}')
+# guests = ['ms.Tikhanovskaya','msr.Chaliy','msr.Bykov','msr.Armen','msr.Georgiy']
+# #guest_person = guests[3]
+# invite_text_intro = 'Dear '
+# invite_text_body = '! I invite you to lounch!'
+# #print(f'{invite_text_intro}{guest_person}{invite_tetxt_body}')
+# print(f'{invite_text_intro}{guests[0]}{invite_text_body}')
+# print(f'{invite_text_intro}{guests[1]}{invite_text_body}')
+# print(f'{invite_text_intro}{guests[2]}{invite_text_body}')
+# print(f'{invite_text_intro}{guests[3]}{invite_text_body}')
+# print(f'{invite_text_intro}{guests[4]}{invite_text_body}')
+
+import keyword
+kw_list = keyword.kwlist
+for kw in kw_list:
+    print(kw)
--- Practiks_2.py
+++ Practiks_2.py
@@ -1,1 +1,18 @@
-print('Твой возраст: '+ str(43*365*24*60) +" минут")
+
+# def truncate(text, length):
+#     result = f"{text[0:length]}..."
+#     return print(result)
+# text = 'Becose we are wery important people! Sad mr. Putin'
+# truncate(text, 24)
+
+def get_hidden_card(card_number, lenght):
+   return(print(f"{'*'*lenght}{card_number[12::]}"))
+
+card_number = str(1253263545858811)
+get_hidden_card(card_number,4)
+
+
+
+
+
+
