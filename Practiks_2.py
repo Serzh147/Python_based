65a90d96f940283c19d297941d92004973078fa5
Serzh147 <79854433620@yandex.ru>
2022-03-14 10:50:57 +0300

    0314

--- Practiks_2.py
+++ Practiks_2.py
@@ -5,14 +5,26 @@
 # text = 'Becose we are wery important people! Sad mr. Putin'
 # truncate(text, 24)
 
-def get_hidden_card(card_number, lenght):
-   return(print(f"{'*'*lenght}{card_number[12::]}"))
+# def get_hidden_card(card_number, lenght):
+#    return(print(f"{'*'*lenght}{card_number[12::]}"))
+#
+# card_number = str(1253263545858811)
+# get_hidden_card(card_number,4)
 
-card_number = str(1253263545858811)
-get_hidden_card(card_number,4)
+numbers = []
+for value in range(1,11):
+   cube = value**3
+   numbers.append(cube)
+print(numbers)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
 
-
-
+print()
+print('Второй способ:')
+print()
+cube = [value**3 for value in range(1,11)]
+print(cube)
 
 
 
